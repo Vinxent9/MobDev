69d9fa85dcfa61b6bb1dffda8c469495bc066822
Vinxent9 <vincentiusvinky98@gmail.com>
2020-04-14 15:08:59 +0800

    add navigation

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native'
+import Comp1 from './component/Comp1'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+//Homescreen navigator function
+const HomeScreen = ({navigation}) => {
+
+  return( 
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
-  );
+  )
+
 }
+
+//detailscreen navigator function
+const DetailScreen = ({navigation}) => {
+
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details again?"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  )
+
+}
+
+const Stack = createStackNavigator()
+
+
+const App = () => {
+  
+  return( 
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name = "Home" component = {HomeScreen} />
+        <Stack.Screen name = "Details" component = {DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +71,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ component/Comp1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {StyleSheet,Text,ScrollView,View} from 'react-native'
+
+const Comp1 = () => {
+
+    return (
+        <View>
+            <Text>This is Comp1</Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  
+  export default Comp1
